fda59144ef328a01b683f9e5dfe8255f570da679
njuclc <njuclc@gmail.com>
2024-04-01 07:44:07 +0800

    feat: 删除仪表盘冗余信息

--- web/src/view/dashboard/index.vue
+++ web/src/view/dashboard/index.vue
@@ -4,120 +4,12 @@
       <div class="gva-card gva-top-card">
         <div class="gva-top-card-left">
           <div class="gva-top-card-left-title">早安，管理员，请开始一天的工作吧</div>
-          <div class="gva-top-card-left-dot">{{ weatherInfo }}</div>
-          <el-row class="my-8 w-[500px]">
-            <el-col
-              :span="8"
-              :xs="24"
-              :sm="8"
-            >
-              <div class="flex items-center">
-                <el-icon class="dashboard-icon">
-                  <sort />
-                </el-icon>
-                今日流量 (1231231)
-              </div>
-            </el-col>
-            <el-col
-              :span="8"
-              :xs="24"
-              :sm="8"
-            >
-              <div class="flex items-center">
-                <el-icon class="dashboard-icon">
-                  <avatar />
-                </el-icon>
-                总用户数 (24001)
-              </div>
-            </el-col>
-            <el-col
-              :span="8"
-              :xs="24"
-              :sm="8"
-            >
-              <div class="flex items-center">
-                <el-icon class="dashboard-icon">
-                  <comment />
-                </el-icon>
-                好评率 (99%)
-              </div>
-            </el-col>
-          </el-row>
-          <div>
-            <div class="gva-top-card-left-item">
-              使用教学：
-              <a
-                style="color:#409EFF"
-                target="view_window"
-                href="https://www.bilibili.com/video/BV1Rg411u7xH/"
-              >https://www.bilibili.com/video/BV1Rg411u7xH</a>
-            </div>
-            <div class="gva-top-card-left-item">
-              插件仓库：
-              <a
-                style="color:#409EFF"
-                target="view_window"
-                href="https://plugin.gin-vue-admin.com/#/layout/home"
-              >https://plugin.gin-vue-admin.com</a>
-            </div>
-          </div>
         </div>
         <img
           src="@/assets/dashboard.png"
           class="gva-top-card-right"
           alt
         >
-      </div>
-    </div>
-    <div class="gva-card-box">
-      <div class="gva-card quick-entrance">
-        <div class="gva-card-title">快捷入口</div>
-        <el-row :gutter="20">
-          <el-col
-            v-for="(card, key) in toolCards"
-            :key="key"
-            :span="4"
-            :xs="8"
-            class="quick-entrance-items"
-            @click="toTarget(card.name)"
-          >
-            <div class="quick-entrance-item">
-              <div
-                class="quick-entrance-item-icon"
-                :style="{ backgroundColor: card.bg }"
-              >
-                <el-icon>
-                  <component
-                    :is="card.icon"
-                    :style="{ color: card.color }"
-                  />
-                </el-icon>
-              </div>
-              <p>{{ card.label }}</p>
-            </div>
-          </el-col>
-        </el-row>
-      </div>
-    </div>
-    <div class="gva-card-box">
-      <div class="gva-card">
-        <div class="gva-card-title">数据统计</div>
-        <div class="p-4">
-          <el-row :gutter="20">
-            <el-col
-              :xs="24"
-              :sm="18"
-            >
-              <echarts-line />
-            </el-col>
-            <el-col
-              :xs="24"
-              :sm="6"
-            >
-              <dashboard-table />
-            </el-col>
-          </el-row>
-        </div>
       </div>
     </div>
   </div>
